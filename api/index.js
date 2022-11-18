b996d98101788cd5e2f988d953920eb120410f1c
ncgames868 <118531058+ncgames868@users.noreply.github.com>
2022-11-18 06:39:20 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,38 @@
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// const users = require('./models/user')
+
+// Routes
+const userRouter = require('../routes/users.router');
+const routergames = require('../routes/routerGames');
+const routerVarious = require('../routes/routerVarious');
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/user', cors(), userRouter);
+app.use('/games', cors(), routergames);
+app.use('/', cors(), routerVarious);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening in: https://localhost:${PORT}`);
+});
+
+server.on('error', (error) => console.log('Error: ', error.message));
+
+mongoose.connect(
+  'mongodb+srv://gandhycoder:RanaMediana@cluster0.9gsyi7q.mongodb.net/?retryWrites=true&w=majority',
+  (err) => {
+    if (err) throw err;
+    console.log('Successfully conected to GamesDB');
+  }
+);
