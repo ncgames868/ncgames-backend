855bdc3c52b10f05e369d70f69b83acfe9a3b353
gandhyman <gandhycoder@gmail.com>
2022-11-21 22:37:57 -0700

    update 2

--- README.md
+++
@@ -1,3 +0,0 @@
-# GRUPO-C8-68-FT-MERN
-
-Este proyecto trata sobre un ecommerce de videojuegos
--- client/src/Components/layout/layout.jsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "./styles";
-
-export const Layout = ({children}) => {
-    return ( 
-        <Container>{children}</Container>
-    );
-}
